2704bf8f2c52a49ee687b8986e1d002c88ba8020
Parmis Morshedi <parmis76.morshedi@gmail.com>
2024-11-05 15:43:37 +0100

    Fixed CommentPage

---
+++ Models/CommentCreateEditDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventureMVC.Models
+{
+    public class CommentCreateEditDTO
+    {
+        [StringLength(2500)]
+        public string? CommentText { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string UserId { get; set; }    
+        public int ActivityId { get; set; }
+    }
+}
